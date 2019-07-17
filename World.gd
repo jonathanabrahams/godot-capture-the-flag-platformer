ce4a9aad48d71c1e89ff25894c5dc3528805106b
Jonathan Abrahams <jonathan.abrahams@hetzner.co.za>
2019-07-17 21:13:43 +0200

    Add Title screen

--- World.gd
+++ World.gd
@@ -1,4 +1,11 @@
 extends Node
 
+func _process(delta):
+	if Input.is_action_just_pressed("ui_cancel"):
+		get_tree().change_scene("res://TitleScreen.tscn")
+
 func _on_Node2D_captured():
 	get_tree().quit()
+
+func _on_ToolButton_pressed():
+	get_tree().change_scene("res://TitleScreen.tscn")
